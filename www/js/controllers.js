da898b548d3ec58e6d370fde8dfafb1421b2535e
zsolt nemeth <furion1@gmail.com>
2016-08-09 17:49:51 -0400

    magazine

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,11 +15,19 @@ function ($scope, $stateParams, $http) {
     });
 }])
 
-.controller('listCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('listCtrl', ['$scope', '$stateParams', '$http',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $http) {
+  $scope.items = [];
 
+  $http.get("http://www.curatist.co:8081/feed/all/50")
+    .success(function (response) {
+      angular.forEach(response, function (item) {
+        $scope.items.push(item);
+      });
+      console.log($scope.items)
+    });
 
 }])
 
